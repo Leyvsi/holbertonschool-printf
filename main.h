8e1e708d89daead4d5635e1aca3e8821328d33f7
leyvsi <sylviecosta78500@gmail.com>
2025-11-25 23:32:18 +0100

    correct main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(char *format, ...);
+int print_char(int c);
+int print_str(char *str);
+int print_digit(int n);
+
+#endif
