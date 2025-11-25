cb6c69b72ddd911f4ed029d4be8a67344674becc
leyvsi <sylviecosta78500@gmail.com>
2025-11-26 00:04:58 +0100

    update final of README.md

--- main.h
+++ main.h
@@ -4,9 +4,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/*main _printf function*/
 int _printf(char *format, ...);
+/*character functions*/
 int print_char(int c);
 int print_str(char *str);
+/*integer functions*/
 int print_digit(int n);
+int print_int(va_list args);
+int print_int_helper(int n);
 
 #endif
